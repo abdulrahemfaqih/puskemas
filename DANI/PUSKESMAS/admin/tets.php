d1ff3936ec306d474b66d0827182aaf71f64ccf6
Abdul Rahem Faqih <120617698+abdulrahemfaqih@users.noreply.github.com>
2023-11-29 21:10:02 +0700

    Delete DANI directory

--- DANI/PUSKESMAS/admin/tets.php
+++
@@ -1,7 +0,0 @@
-<select class="form-select" aria-label="Default select example" name="pelayan">
-    <?php foreach ($pelayan as $sp): ?>
-        <option value="<?= $sp ?>" <?= ($order['pelayan'] == $sp) ? 'selected' : ''; ?>>
-            <?= $sp ?>
-        </option>
-    <?php endforeach; ?>
-</select>
